c620f9a0d2685619a3190067b4b566242f4ec6ee
developer <codesharing@gmx.de>
2018-08-10 18:41:35 +0200

    -

--- src/vctrainer/VCTrainer.java
+++ src/vctrainer/VCTrainer.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vctrainer;
 
 import javafx.application.Application;
@@ -10,19 +6,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import view.GetFXMLFiles;
 
-/**
- *
- * @author Big Boss
- */
+ 
+
+
 public class VCTrainer extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        GetFXMLFiles fxml = new GetFXMLFiles();
+        Parent root = fxml.getFXML();
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Vokabeltrainer 2018");
         stage.setScene(scene);
         stage.show();
     }
